d04b3dadbf95be1eb634b939bc2ca7690ba80e96
Luca Sasselli <4eversax@gmail.com>
2017-04-12 17:09:40 +0200

    Program code cleanup

--- SmartLock/Program.cs
+++ SmartLock/Program.cs
@@ -1,49 +1,41 @@
 ﻿using System;
-using System.Collections;
-using System.Threading;
-
 using Microsoft.SPOT;
-
-using GHI.Glide;
-using GHI.Glide.Display;
-using GT = Gadgeteer; 
-using Gadgeteer.Modules.GHIElectronics;
-using Gadgeteer.Modules.Luca_Sasselli;
 using SmartLock.GUI;
+using GT = Gadgeteer;
 
 namespace SmartLock
 {
     public partial class Program
     {
         // Time constants
-        private const int WINDOW_ACCESS_PERIOD = 2000;
-        private const int WINDOW_ALERT_PERIOD = 10000;
-        private const int NFC_SCAN_PERIOD = 1000;
-        private const int NFC_SCAN_TIMEOUT = 200;
+        private const int WindowAccessPeriod = 2000;
+        private const int WindowAlertPeriod = 10000;
+        private const int NfcScanPeriod = 1000;
+        private const int NfcScanTimeout = 200;
 
         // Window ID constants
-        private const int WINDOW_PIN_ID = 0;
-        private const int WINDOW_ACCESS_ID = 1;
-        private const int WINDOW_ALERT_ID = 2;
-        private const int WINDOW_SCAN_ID = 3;
-        
-        // Main Objects
-        private PinWindow pinWindow;
-        private AccessWindow accessWindow;
-        private AlertWindow scanWindow;
-        private DataHelper dataHelper;
+        private const int WindowPinId = 0;
+        private const int WindowAccessId = 1;
+        private const int WindowAlertId = 2;
+        private const int WindowScanId = 3;
 
         // Nfc setup
         private string pendingPin;
 
+        // Main Objects
+        private DataHelper dataHelper;
+        private AccessWindow accessWindow;
+        private PinWindow pinWindow;
+        private AlertWindow scanWindow;
+
         public void ProgramStarted()
         {
-            Debug.Print("Program started!");          
+            Debug.Print("Program started!");
 
             // Object init
-            pinWindow = new PinWindow(WINDOW_PIN_ID);
-            accessWindow = new AccessWindow(WINDOW_ACCESS_ID, WINDOW_ACCESS_PERIOD);
-            scanWindow = new AlertWindow(WINDOW_SCAN_ID, WINDOW_ALERT_PERIOD);
+            pinWindow = new PinWindow(WindowPinId);
+            accessWindow = new AccessWindow(WindowAccessId, WindowAccessPeriod);
+            scanWindow = new AlertWindow(WindowScanId, WindowAlertPeriod);
             dataHelper = new DataHelper(ethernetJ11D, sdCard);
 
             // Event Setup
@@ -53,12 +45,10 @@ namespace SmartLock
 
             // Set scan window
             scanWindow.SetText("Please scan your NFC card now...");
-            scanWindow.SetNegativeButton("Cancel", delegate(Object target)
-            {
-                scanWindow.Dismiss();
-            });
+            scanWindow.SetNegativeButton("Cancel", delegate { scanWindow.Dismiss(); });
 
             dataHelper.Init();
+            adafruit_PN532.Init();
 
             WindowManger.MainWindow = pinWindow;
             WindowManger.WindowChanged += WindowChanged;
@@ -70,12 +60,12 @@ namespace SmartLock
          * This event occurs when the user passes a NFC card near the reader.
          * It checks if the UID is valid and unlock the door if so.
          */
-        void TagFound(string uid)
+        private void TagFound(string uid)
         {
             if (!scanWindow.IsShowing())
             {
                 // Check authorization
-                bool authorized = dataHelper.CheckCardID(uid);
+                var authorized = dataHelper.CheckCardID(uid);
 
                 // Show access window
                 accessWindow.Show(authorized);
@@ -95,30 +85,27 @@ namespace SmartLock
                 }
 
                 Debug.Print(logText);
-                Log accessLog = new Log(Log.TYPE_ACCESS, uid, logText, DateTime.Now.ToString());
+                var accessLog = new Log(Log.TYPE_ACCESS, uid, logText, DateTime.Now.ToString());
                 dataHelper.AddLog(accessLog); //add log to log list
             }
             else
             {
                 // Log new cardID
-                Log newCardIDLog = new Log(
-                    Log.TYPE_ACCESS, 
-                    pendingPin, 
-                    uid, 
-                    "Card \"" + uid + "\" has been added for pin \"" + pendingPin + "\".", 
+                var newCardIdLog = new Log(
+                    Log.TYPE_ACCESS,
+                    pendingPin,
+                    uid,
+                    "Card \"" + uid + "\" has been added for pin \"" + pendingPin + "\".",
                     DateTime.Now.ToString());
 
                 // Update CardID in userList
                 dataHelper.AddCardID(pendingPin, uid);
 
-                dataHelper.AddLog(newCardIDLog);
+                dataHelper.AddLog(newCardIdLog);
 
-                AlertWindow cardAddedAlert = new AlertWindow(WINDOW_ALERT_ID, WINDOW_ALERT_PERIOD);
+                var cardAddedAlert = new AlertWindow(WindowAlertId, WindowAlertPeriod);
                 cardAddedAlert.SetText("NFC card added!");
-                cardAddedAlert.SetPositiveButton("Ok", delegate(Object target)
-                {
-                    cardAddedAlert.Dismiss();
-                });
+                cardAddedAlert.SetPositiveButton("Ok", delegate { cardAddedAlert.Dismiss(); });
                 cardAddedAlert.Show();
             }
         }
@@ -128,11 +115,11 @@ namespace SmartLock
          * This event occurs when the user inserts a pin code. It checks if the pin is valid and unlock the door if so.
          * If the pin has no related CardId it prompts the user to add it.
          */
-        void PinFound(string pin)
+        private void PinFound(string pin)
         {
             // Check authorization
-            bool authorized = dataHelper.CheckPin(pin);
-            bool nullCardID = dataHelper.PinHasNullCardID(pin);
+            var authorized = dataHelper.CheckPin(pin);
+            var nullCardId = dataHelper.PinHasNullCardID(pin);
 
             // Log the event
             string logText;
@@ -150,27 +137,28 @@ namespace SmartLock
 
 
             Debug.Print(logText);
-            Log accessLog = new Log(Log.TYPE_ACCESS, pin, logText, DateTime.Now.ToString());
+            var accessLog = new Log(Log.TYPE_ACCESS, pin, logText, DateTime.Now.ToString());
             dataHelper.AddLog(accessLog); //add log to log list
 
-            if (nullCardID)
+            if (nullCardId)
             {
                 // Null CardID detected, prompt the user to set one
-                AlertWindow nullCardIDAlert = new AlertWindow(WINDOW_ALERT_ID, WINDOW_ALERT_PERIOD);
-                nullCardIDAlert.SetText("It happears that this user has no related NFC card.\nDo you want to scan it now?");
-                nullCardIDAlert.SetPositiveButton("Yes", delegate(Object target)
+                var nullCardIdAlert = new AlertWindow(WindowAlertId, WindowAlertPeriod);
+                nullCardIdAlert.SetText(
+                    "It happears that this user has no related NFC card.\nDo you want to scan it now?");
+                nullCardIdAlert.SetPositiveButton("Yes", delegate
                 {
                     // User wants to add a new NFC card
                     pendingPin = pin;
-                    nullCardIDAlert.StopTimer(); // Hacky solution, but prevents graphical glitches
+                    nullCardIdAlert.StopTimer(); // Hacky solution, but prevents graphical glitches
                     scanWindow.Show();
                 });
-                nullCardIDAlert.SetNegativeButton("No", delegate(Object target)
+                nullCardIdAlert.SetNegativeButton("No", delegate
                 {
                     // User doesn't want to add a new NFC card
-                    nullCardIDAlert.Dismiss();
+                    nullCardIdAlert.Dismiss();
                 });
-                nullCardIDAlert.Show();
+                nullCardIdAlert.Show();
             }
             else
             {
@@ -183,7 +171,7 @@ namespace SmartLock
          * UNLOCK DOOR
          * Called by either PinFound or TagFound to unlock the door.
          */
-        void UnlockDoor()
+        private void UnlockDoor()
         {
             //TODO
         }
@@ -192,17 +180,15 @@ namespace SmartLock
          * DATA SOURCE CHANGED EVENT:
          * This event occurs when the data source for the user list changes.
          */
-        void DataSourceChanged(int dataSource)
+        private void DataSourceChanged(int dataSource)
         {
             pinWindow.SetDataSource(dataSource);
             if (dataSource == DataHelper.DATA_SOURCE_ERROR)
             {
-                AlertWindow dataSourceAlert = new AlertWindow(WINDOW_ALERT_ID, WINDOW_ALERT_PERIOD);
-                dataSourceAlert.SetText("Unable to load dataset from cache! The system will remain offline until connection is established.");
-                dataSourceAlert.SetPositiveButton("Ok", delegate(Object target) 
-                {
-                    dataSourceAlert.Dismiss();
-                });
+                var dataSourceAlert = new AlertWindow(WindowAlertId, WindowAlertPeriod);
+                dataSourceAlert.SetText(
+                    "Unable to load dataset from cache! The system will remain offline until connection is established.");
+                dataSourceAlert.SetPositiveButton("Ok", delegate { dataSourceAlert.Dismiss(); });
                 dataSourceAlert.Show();
             }
         }
@@ -211,19 +197,12 @@ namespace SmartLock
          * PIN WINDOW VISIBLE EVENT:
          * This event occours when the visibility of pinWindow changes.
          */
-
-        void WindowChanged(int ID)
+        private void WindowChanged(int id)
         {
-            if (ID == WINDOW_PIN_ID || ID == WINDOW_SCAN_ID)
-            {
-                adafruit_PN532.StartScan(NFC_SCAN_PERIOD, NFC_SCAN_TIMEOUT);
-            }
+            if (id == WindowPinId || id == WindowScanId)
+                adafruit_PN532.StartScan(NfcScanPeriod, NfcScanTimeout);
             else
-            {
-                // Pin window not visible
                 adafruit_PN532.StopScan();
-            }
         }
     }
 }
-
